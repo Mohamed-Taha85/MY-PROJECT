ae0dac1030be24d5687bf1eb7588a2b4e21abe7c
Mohamed Taha <mo.mediouni@aui.ma>
2025-06-24 13:50:16 +0100

    Update database config for Railway and ensure pymysql is included

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:@localhost/hse_db'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:oJjHNDoNfOcIeXRHPCwQaRVCQOiatorz@mysql.railway.internal:3306/railway'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'ta_cle_secrete'
+    SECRET_KEY = 'ta_cle_secrete'  # Change-la par une vraie clé secrète forte
